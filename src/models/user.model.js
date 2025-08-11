c2c22da02823a11ab2df4f8e9c7fa430188c3f14
Enmanuel Urbina <enmanuelu100@gmail.com>
2025-08-11 14:47:57 -0600

    agregando tipos y requerimientos en schema usuario

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,27 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  name: String,
-  passwordHash: String
+  username: {
+    type: String,
+    unique: true,
+    require: true
+  },
+  name: {
+    type: String,
+    require: true
+  },
+  passwordHash: {
+    type: String,
+    require: true
+  }
 }, { collection: 'user' })
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject.__v
+    delete returnedObject._id
+  }
+})
 
 export const UserModel = mongoose.model('User', userSchema)
